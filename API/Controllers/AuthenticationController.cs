ce36773813fb9b8984786a0df26c4384b31eda6f
KirillKutas <kutaskirillbstu@gmail.com>
2020-10-19 17:52:13 +0300

    create template

---
+++ API/Controllers/AuthenticationController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Application.Authentication.Commands;
+using Application.Common.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AuthenticationController : BaseController
+    {
+        private readonly ITokenService _tokenService;
+
+        public AuthenticationController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<string>> Login([FromBody] LoginCommand command)
+        {
+            await Mediator.Send(command);
+            
+            // append security header
+            _tokenService.AppendSecurityToken();
+            return Ok();
+        }
+
+        [HttpPost("logout")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Logout()
+        {
+            return await Task.FromResult(Ok());
+        }
+
+    }
+}
---
+++ Application/Authentication/Commands/LoginCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Authentication;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+namespace Application.Authentication.Commands
+{
+    public class LoginCommand : IRequest
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class LoginCommandHandler : AsyncRequestHandler<LoginCommand>
+    {
+        
+        protected override async Task Handle(LoginCommand command, CancellationToken cancellationToken)
+        {
+            // TODO: check for login do something if needed
+            var credentialsIsValid = await Task.FromResult(command.UserName != null && command.Password != null);
+
+            if (!credentialsIsValid)
+            {
+                throw new InvalidCredentialException();
+            }
+        }
+    }
+}
